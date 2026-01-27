f6ec1db9e887f4f4b454625c34ef63bc1d7d8536
eduz <43731256+eduz1@users.noreply.github.com>
2026-01-26 18:46:30 -0600

    Optimization fix, arguments for patching

--- IPSPatcherUI/main.cpp
+++ IPSPatcherUI/main.cpp
@@ -1,9 +1,31 @@
 #include "stdafx.h"
 #include "IPSPatcherUI.h"
+#include "IPSPatcherCore.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
+    // We can also expect the file to be patched, the IPS file, and optionally 
+    // the output file path
+    if (argc == 4)
+    {
+        const std::string filePath = argv[1];
+        const std::string ipsPath = argv[2];
+        const std::string outPath = argv[3];
+
+        if (PatchFile(filePath, ipsPath, outPath))
+        {
+            printf("Patching completed successfully.");
+            return 1;
+        }
+        else
+        {
+			printf("Patching failed.");
+            return 0;
+        }
+    }
+
+    // GUI mode
     QApplication app(argc, argv);
     IPSPatcherUI window;
     window.show();
